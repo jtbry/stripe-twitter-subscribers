f48abf5bff587dbdc15596d239260b11513d4a1b
Justin Bryant <justintbry@gmail.com>
2023-11-21 12:19:48 -0500

    feat: add stripe subscription purchasing and management

--- client/src/views/Subscription.tsx
+++ client/src/views/Subscription.tsx
@@ -1,31 +1,98 @@
 import { User } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { auth } from "../services/firebase.config";
 
 interface SubscriptionViewProps {
   user: User;
 }
 
+interface SubscriptionStatus {
+  status: string;
+  portalUrl: string;
+}
+
 const SubscriptionView = (props: SubscriptionViewProps): JSX.Element => {
   const { user } = props;
+  const [success] = useState(
+    new URLSearchParams(window.location.search).get("success")
+  );
+
+  const [subscriptionStatus, setSubscriptionStatus] =
+    useState<SubscriptionStatus>();
 
   const signOut = () => {
     auth.signOut();
   };
 
-  return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <h1 className="text-3xl">Greetings, {user.displayName}</h1>
-      <p>Your email is {user.email}</p>
-      <p>Your uid is {user.uid}</p>
-      <p>Your photo is {user.photoURL}</p>
+  useEffect(() => {
+    const getSubscriptionStatus = async () => {
+      const token = await user.getIdToken();
+      const res = await fetch("/api/checkSubscriptionStatus?token=" + token, {
+        method: "GET",
+      });
 
-      <button
-        onClick={signOut}
-        className="w-1/4 rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-      >
-        Sign Out
-      </button>
-    </div>
+      const data = await res.json();
+      setSubscriptionStatus(data);
+    };
+
+    if (user) {
+      getSubscriptionStatus();
+    }
+  }, [user]);
+
+  if (success) {
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 3000);
+  }
+
+  let managementElement = <></>;
+  if (subscriptionStatus?.status && subscriptionStatus.status !== "none") {
+    managementElement = (
+      <>
+        <h2>Subscription Status: {subscriptionStatus.status}</h2>
+        <a
+          className="w-1/4 rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          href={subscriptionStatus.portalUrl}
+        >
+          Manage Subscription
+        </a>
+      </>
+    );
+  } else {
+    managementElement = (
+      <form action="/api/subscribe" className="py-4">
+        {user.email && <input type="hidden" name="email" value={user.email} />}
+        <button className="w-1/4 rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+          Subscribe
+        </button>
+      </form>
+    );
+  }
+
+  return (
+    <>
+      {success && (
+        <div className="bg-green-600 text-white p-4">
+          Success! Thanks for subscribing.
+        </div>
+      )}
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <h1 className="text-3xl">Greetings, {user.displayName}</h1>
+        <p>Your email is {user.email}</p>
+        <p>Your uid is {user.uid}</p>
+        <p>Your photo is {user.photoURL}</p>
+
+        <div className="py-4">{managementElement}</div>
+
+        <button
+          onClick={signOut}
+          className="w-1/4 rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+        >
+          Sign Out
+        </button>
+      </div>
+    </>
   );
 };
 
---
+++ functions/src/index.ts
@@ -0,0 +1,101 @@
+import { QuerySnapshot } from "firebase-admin/firestore";
+import { defineSecret, defineString } from "firebase-functions/params";
+import { onRequest } from "firebase-functions/v2/https";
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const { getAuth } = require("firebase-admin/auth");
+
+initializeApp();
+
+const subscriptionPriceId = defineString("SUBSCRIPTION_PRICE_ID");
+const frontendUrl = defineString("FRONTEND_URL");
+const stripeApiKey = defineSecret("STRIPE_API_KEY");
+
+export const subscribe = onRequest(
+  { secrets: [stripeApiKey] },
+  async (request, response) => {
+    const stripe = require("stripe")(stripeApiKey.value());
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: subscriptionPriceId.value(),
+          quantity: 1,
+        },
+      ],
+      success_url: `${frontendUrl.value()}?success=true`,
+      cancel_url: `${frontendUrl.value()}`,
+      customer_email: request.query.email,
+    });
+
+    response.redirect(303, session.url);
+  }
+);
+
+export const handleStripeEvents = onRequest(async (request, response) => {
+  const event = request.body;
+
+  // Handle the event
+  switch (event.type) {
+    case "customer.subscription.updated":
+      const subscription = event.data.object;
+      await getFirestore()
+        .collection("subscriptions")
+        .where("customerId", "==", subscription.customer)
+        .get()
+        .then((snapshot: QuerySnapshot) => {
+          snapshot.docs.forEach((doc) => {
+            doc.ref.update({
+              status: subscription.status,
+            });
+          });
+        });
+      break;
+    case "customer.subscription.deleted":
+      const deleteTarget = event.data.object;
+      await getFirestore()
+        .collection("subscriptions")
+        .where("customerId", "==", deleteTarget.customer)
+        .get()
+        .then((snapshot: QuerySnapshot) => {
+          snapshot.docs.forEach((doc) => {
+            doc.ref.delete();
+          });
+        });
+      break;
+    case "customer.created":
+      const customer = event.data.object;
+      await getFirestore().collection("subscriptions").add({
+        email: customer.email,
+        customerId: customer.id,
+        status: "incomplete",
+      });
+      break;
+  }
+
+  response.json({ received: true });
+});
+
+export const checkSubscriptionStatus = onRequest(
+  { secrets: [stripeApiKey] },
+  async (request, response) => {
+    const user = await getAuth().verifyIdToken(request.query.token);
+    const subscription: QuerySnapshot = await getFirestore()
+      .collection("subscriptions")
+      .where("email", "==", user.email)
+      .get();
+
+    if (subscription.empty) {
+      response.json({ status: "none" });
+    } else {
+      const status = subscription.docs[0].data().status;
+      const stripe = require("stripe")(stripeApiKey.value());
+      const portal = await stripe.billingPortal.sessions.create({
+        customer: subscription.docs[0].data().customerId,
+        return_url: `${frontendUrl.value()}`,
+      });
+      response.json({ status, portalUrl: portal.url });
+    }
+  }
+);
