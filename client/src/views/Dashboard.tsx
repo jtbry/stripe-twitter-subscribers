557f034814aa096d348ae0566aba67056441cb87
Justin Bryant <justintbry@gmail.com>
2023-11-30 14:30:05 -0500

    feat: dashboard page loading notice

--- client/src/views/Dashboard.tsx
+++ client/src/views/Dashboard.tsx
@@ -76,6 +76,12 @@ const DashboardView = (props: DashboardProps): JSX.Element => {
               Subscribe Now
             </a>
           )}
+
+          {!subscribeUrl && !subscription && (
+            <>
+              <p>Loading...</p>
+            </>
+          )}
           <button
             onClick={signOut}
             className="mt-4 w-full flex justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
